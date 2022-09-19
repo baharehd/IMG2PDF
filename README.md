c530fc09b08363404575e282e3945e48baf772af
bahareh <bahareh97@gmail.com>
2022-09-18 23:27:27 -0400

    Added folders for image input and file output

--- README.md
+++ README.md
@@ -2,11 +2,16 @@
 Bahareh Doroudiani
 
 IMG2PDF is a script to stitch and convert image files into PDF.
-This is a personal project to learn and practice version control.
+
+This is a personal project to learn and practice version control and good design.
 There are converters already online, 
 however I want to make my own converter because I don't want to upload sensitive images to online converters.
 
 ## Requirements
+<!---
+Figure out which version of python I'm using 
+(prlly 3, but idk if it needs to be more specific like 3.7) 
+-->
 Install Pillow library for Python.
 ```bash
 pip install Pillow
--- main.py
+++ main.py
@@ -7,14 +7,21 @@ i guess describe variables and types here
 and then describe functions here
 """
 
-import PIL
+from PIL import Image
 
 # TODO: global variables
+# image locations,
+source_path = 'test_images\\'
+output_path = 'test_output\\'
 
 # TODO: inputs
+# every user will want to input at least 1 image;
+# figure out if i need to add extra options later
+image = Image.open(source_path, "r")  # testing one image for now
 
 # TODO: make sure image formats are ok/won't crash the script (create separate script to make functions?)
 
 # TODO: need to figure out how to stitch images so that each image would be a separate page in the pdf
 
 # TODO: save/export final file! Yay!
+image.save(output_path, "pdf")
